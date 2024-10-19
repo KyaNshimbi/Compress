023c5cac7ddadce458041bc3117fc2e0bce2b746
Kyakilika Nshimbi <kyanshimbi@gmail.com>
2024-10-19 18:00:51 +0200

    Adding OOP

--- Compress/src/main.cpp
+++ Compress/src/main.cpp
@@ -8,7 +8,103 @@ class PNGCompress
 private:
 	png_bytepp rowPointers;
 	png_uint_32 width;
+	png_uint_32 height;
+	png_byte colorType;
+	png_byte bitDepth;
+	
+	FILE* readingFPtr;
 
+	png_structp readingStruct;
+	png_infop readingInfoPtr;
+public:
+	PNGCompress()
+		: rowPointers{ nullptr }, width{ 0 }, height{ 0 }, colorType{ 0 }, bitDepth{ 0 }, readingFPtr{ nullptr }, readingStruct{ nullptr }, readingInfoPtr{ nullptr } {}
+
+	PNGCompress(const char* fileName)
+		: rowPointers{ nullptr }, width{ 0 }, height{ 0 }, colorType{ 0 }, bitDepth{ 0 }, readingFPtr{ nullptr }, readingStruct{ nullptr }, readingInfoPtr{ nullptr }
+	{
+		if (!open(fileName))
+		{
+			abort();
+		}
+	}
+
+	bool open(const char* fileName)
+	{
+		fopen_s(&readingFPtr, fileName, "rb");
+		if (!readingFPtr)
+		{
+			return false;
+		}
+		return true;
+	}
+	~PNGCompress()
+	{
+		fclose(readingFPtr);
+		png_destroy_read_struct(&readingStruct, &readingInfoPtr, nullptr);
+	}
+	void initialise()
+	{
+		if (!readingFPtr)
+		{
+			abort();
+		}
+
+		//Checking if the first 8 bytes have been read successfully
+		size_t number{ 8 };
+		png_byte header[8];
+		size_t read = fread(header, sizeof(header[0]), number, readingFPtr);
+		if (read != number)
+		{
+			abort();
+		}
+		//Checking if the bytes are corresponding to the png header signature
+		if (png_sig_cmp(header, 0, number))
+		{
+			abort();
+		}
+
+		//Initialising the PNG Structure
+		readingStruct =
+			png_create_read_struct(PNG_LIBPNG_VER_STRING, nullptr,nullptr, nullptr);
+		if (!readingStruct)
+		{
+			abort();
+		}
+		//Initialising PNG Info
+		readingInfoPtr =  png_create_info_struct(readingStruct);
+		if (!readingInfoPtr)
+		{
+			png_destroy_read_struct(&readingStruct, nullptr, nullptr);
+			abort();
+		}
+		//If libpng encounteres an error a long jump
+		if (setjmp(png_jmpbuf(readingStruct)))
+		{
+			png_destroy_read_struct(&readingStruct, &readingInfoPtr, nullptr);
+			abort();
+		}
+
+		//Initialising the pngIO to defaults
+		png_init_io(readingStruct, readingFPtr);
+
+		//Notifying libpng that we checked the signature of the png file and some bytes are missing from the start of the file
+		png_set_sig_bytes(readingStruct, number);
+
+		//High level read interface
+		png_read_png(readingStruct, readingInfoPtr, PNG_TRANSFORM_IDENTITY, nullptr);
+
+		rowPointers = png_get_rows(readingStruct, readingInfoPtr);
+		width = png_get_image_width(readingStruct, readingInfoPtr);
+		height = png_get_image_height(readingStruct, readingInfoPtr);
+		colorType = png_get_color_type(readingStruct, readingInfoPtr);
+		bitDepth = png_get_bit_depth(readingStruct, readingInfoPtr);
+	}
+
+	void Compress()
+	{
+
+	}
 };
 void readPNGFile(const char* fileName, png_bytepp& rowPointers, png_uint_32& width, png_uint_32& height, png_byte& colorType, png_byte& bitDepth)
 {
@@ -126,6 +222,7 @@ int main(int argc, char* argv[])
 	png_uint_32 height{ 0 };
 	png_byte colorType{};
 	png_byte bitDepth{};
+
 	if (argc > 1)
 	{
 		readPNGFile(argv[1], rowPointers, width, height, colorType, bitDepth);
